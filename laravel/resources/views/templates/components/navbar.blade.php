6506f3386de6ed28a322ebe2f089c5510a7c03f1
DickyAdi <dickyadi44@gmail.com>
2022-06-07 14:59:21 +0700

    Home view done

---
+++ laravel/resources/views/templates/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <a href="#" class="navbar-brand"><img src="img/group 30.png" height="50"></a>
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item mr-2">
+                    <a class="nav-link"  href="#">Home</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="nav-link" href="#">Rumah Sakit terdekat</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="nav-link" href="#">Apotik terdekat</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Konsultasi</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
